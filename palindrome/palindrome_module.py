d867e9e586cb01891f2b0710717d94146006928e
BTamara <bolyatami@gmail.com>
2016-05-23 14:23:45 +0200

    done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,8 +1,23 @@
+import string
+
+
 def palindrome(str):
-    return
+    str = str.lower()
+    test_str = ''.join(str.split())
+    temp = []
+    for c in test_str:
+        temp.append(c)
+    temp.reverse()
+    test_str_reversed = ''.join(temp)
+    if test_str == test_str_reversed:
+        return(True)
+    else:
+        return(False)
 
 
 def main():
+    pal_str = input('Gimme a string, human! ')
+    print(palindrome(pal_str))
     return
 
 
