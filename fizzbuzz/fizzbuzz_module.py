958ed636314622883c07a118d64ae51149d5e7e1
BTamara <bolyatami@gmail.com>
2016-05-23 13:17:30 +0200

    working

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,16 @@
 def fizzbuzz(number):
-    return
-
+    if number % 3 == 0 and number % 5 == 0:
+        return("FizzBuzz")
+    elif number % 5 == 0:
+        return("Buzz")
+    elif number % 3 == 0:
+        return("Fizz")
+    else:
+        return(number)
 
 def main():
+    for a in range(1, 101):
+        print(fizzbuzz(a))
     return
 
 if __name__ == '__main__':
