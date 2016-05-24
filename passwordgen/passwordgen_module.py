b2b009eec2a2e926308170f573b3b958b2f9f3bf
BTamara <bolyatami@gmail.com>
2016-05-24 10:01:07 +0200

    working

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,23 @@
+import random
+import string
+
+
 def passwordgen():
-    return
+    list_c = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?"]
+    passw = []
+
+    for i in range(2):
+        passw.append(random.choice(list_c))
+        passw.append(random.choice(string.digits))
+        passw.append(random.choice(string.ascii_lowercase))
+        passw.append(random.choice(string.ascii_uppercase))
+    random.shuffle(passw)
+    passw_return = ''.join(passw)
+    return(passw_return)
 
 
 def main():
+    print(passwordgen())
     return
 
 
